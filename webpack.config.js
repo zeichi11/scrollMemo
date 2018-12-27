68752bceffdeabd012189da8c803a8443ed261ca
kimjh <kimjh@hancom.com>
2018-12-27 15:53:59 +0900

    preset list 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
 
     output: {
-        path: __dirname + '/public/',
+        path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
     },
 
