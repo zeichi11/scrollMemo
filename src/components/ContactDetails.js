5cf24ce241f9177f032fcbaddf4ec3b7b2c5c4bc
kimjh <kimjh@hancom.com>
2018-12-31 17:12:38 +0900

    contact 수정 기능 추가 - input Toggle을 통한 eidt 모드 활성/비활성화 - Contact 컴포넌트의 handleEdit을 이용하여  input으로 입력받은 내용들로 업데이트 - contact가 선택되지 않은 경우 버튼을 disable 처리하도록 코드 추가 - isEdit에 따라 onClick 시 동작하는 함수가 다르게  설정되도록 코드 추가

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -1,6 +1,48 @@
 import React from 'react';
 
 class ContactDetails extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEdit: false,
+            name: '',
+            phone: ''
+        };
+
+        this.handleToggle = this.handleToggle.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+    }
+
+    /**
+     * Edit 버튼의 toggle값을 설정한다.
+     */
+    handleToggle() {
+        this.setState({
+            isEdit: !this.state.isEdit,
+            name: this.props.contact.name,
+            phone: this.props.contact.phone
+        });
+    }
+
+    /**
+     * input의 값 변경 시 state를 설정한다.
+     * @param e
+     */
+    handleChange(e) {
+        let newState = {};
+        // 이벤트 target의 name 속성을 프로퍼티로 갖는 값을 추가한다.
+        newState[e.target.name] = e.target.value;
+        this.setState(newState);
+    }
+
+    /**
+     * props의 onEdit 함수를 통해 내용을 업데이트 한다.
+     */
+    handleEdit() {
+        this.props.onEdit(this.state.name, this.state.phone)
+    }
+
     render() {
         const details = (
             <div>
@@ -8,12 +50,40 @@ class ContactDetails extends React.Component {
                 <p>{this.props.contact.phone}</p>
             </div>
         );
-        const blank = (<div></div>);
+        const blank = (<div>Not Selected.</div>);
+        const editInput = (
+            <div>
+                <p><input type="text"
+                          name="name"
+                          placeholder="name"
+                          value={this.state.name}
+                          onChange={this.handleChange}/>
+                </p>
+                <p>
+                    <input type="text"
+                           name="phone"
+                           placeholder="phone"
+                           value={this.state.phone}
+                           onChange={this.handleChange}/>
+                </p>
+            </div>
+        );
+        const editView = this.state.isEdit ? editInput : details;
 
         return (
             <div>
                 <h2>Details</h2>
-                {this.props.isSelected ? details : blank}
+                {this.props.isSelected ? editView : blank}
+                <p>
+                    <button disabled={this.props.isSelected ? false : true}
+                            onClick={this.state.isEdit ? this.handleEdit : this.handleToggle}>
+                        {this.state.isEdit ? "OK" : "Edit"}
+                    </button>
+                    <button disabled={this.props.isSelected ? false : true}
+                            onClick={this.props.onRemove}>
+                        Remove
+                    </button>
+                </p>
             </div>
         );
     }
@@ -23,7 +93,9 @@ ContactDetails.defaultProps = {
     contact: {
         name: '',
         phone: ''
-    }
+    },
+    onRemove: () => { console.error("onRemove not defined"); },
+    onEdit: () => { console.error("onEdit not defined"); }
 };
 
 export default ContactDetails;
