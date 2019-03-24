d229509d1bdec4032ef50c53318b75d1cf30dd4b
zeichi <zeichi@naver.com>
2019-03-24 11:50:05 +0900

    bebel-preset-stage-0 설치

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
                 exclude: /node_modules/,
                 query: {
                     cacheDirectory: true,
-                    presets: ['env', 'react']
+                    presets: ['env', 'react', 'stage-0']
                 }
             }
         ]
