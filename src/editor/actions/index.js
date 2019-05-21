fce5da53449be7652f4bc536f1e78c8be0f0b623
zeichi <zeichi@naver.com>
2019-05-22 00:12:21 +0900

    [editor] 에디터 작업
    [내용]
    - reducer에 사용될 action 추가
    - constants 추가

---
+++ src/editor/actions/index.js
@@ -0,0 +1,43 @@
+import Constants from '../common/Constants';
+
+export function updateLineHeight(id, height) {
+	return {
+		type: Constants.action.UPDATE_LINE_HEIGHT,
+		id: id,
+		height: height
+	};
+}
+
+export function insertParagraph() {
+	return {
+		type: Constants.action.INSERT_PARAGRAPH
+	};
+}
+
+export function deleteParagraph() {
+	return {
+		type: Constants.action.DELETE_PARAGRAPH,
+	}
+}
+
+
+export function insertFormat() {
+	return {
+		type: Constants.action.INSERT_FORMAT
+	}
+}
+
+export function deleteFormat() {
+	return {
+		type: Constants.action.DELETE_FORMAT
+	}
+}
+
+export function updateSelection(top, left, height) {
+	return {
+		type: Constants.action.UPDATE_SELECTION,
+		top : top,
+		left: left,
+		height: height
+	}
+}
